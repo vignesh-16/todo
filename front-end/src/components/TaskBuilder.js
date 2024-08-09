c42b254042fe5c7af12480a4f5f5bf8a848283cb
vignesh-16 <assassincreedlll07@gmail.com>
2024-08-09 21:33:43 +0530

    [To Do]:: Front-end: -> Day 3 of implementing feature rich task creation model.

--- front-end/src/components/TaskBuilder.js
+++ front-end/src/components/TaskBuilder.js
@@ -16,15 +16,17 @@ const TaskBuilder = ({ show, onClose, onSave }) => {
             <div className='modal-content'>
                 <span className="close" onClick={onClose}>&times;</span>
                 <form className="new-task-builder" onSubmit={ (e)=> { addTask(e) } }>
-                    <input type="text" placeholder="Enter task title" value={taskName} required onChange={(e)=> { setTaskName(e.target.value)} } />
-                    <fieldset>
-                        <input type="checkbox" className="priority-selector" value={"bucket-list"} aria-label="bucket-list"></input>
-                        <input type="checkbox" className="priority-selector" value={"low"} aria-label="low-priority"></input>
-                        <input type="checkbox" className="priority-selector" value={"medium"} aria-label="medium-priority"></input>
-                        <input type="checkbox" className="priority-selector" value={"high"} aria-label="high-priority"></input>
-                        <input type="checkbox" className="priority-selector" value={"important"} aria-label="absolutely-required"></input>
-                    </fieldset>
-                    <div className="time-limit">
+                    <div className="data-parent title-holder">
+                        <input type="text" className='' placeholder="Enter task title" value={taskName} required onChange={(e)=> { setTaskName(e.target.value)} } />
+                    </div>
+                    <div className="data-parent priorities-holder">
+                        <input type="checkbox" className="priority-selector grow" value={"bucket-list"} aria-label="bucket-list"></input>
+                        <input type="checkbox" className="priority-selector grow" value={"low"} aria-label="low-priority"></input>
+                        <input type="checkbox" className="priority-selector grow" value={"medium"} aria-label="medium-priority"></input>
+                        <input type="checkbox" className="priority-selector grow" value={"high"} aria-label="high-priority"></input>
+                        <input type="checkbox" className="priority-selector grow" value={"important"} aria-label="absolutely-required"></input>
+                    </div>
+                    <div className="data-parent time-limit">
                         <input type="date" className="calendar" required></input>
                     </div>
                     <button type='submit' className='save-task-button'>Create Task</button>
